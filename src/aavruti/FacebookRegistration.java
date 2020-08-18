f342a0453f7a07789a6ae977ee3f691a1f5fa1c2
Aavruti Dalmia <aavruti14@gmail.com>
2020-08-18 23:22:14 +0530

    Aavruti_Assignment_3_AddedSomeValidation

--- src/aavruti/FacebookRegistration.java
+++ src/aavruti/FacebookRegistration.java
@@ -2,7 +2,6 @@ package aavruti;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -20,7 +19,7 @@ public class FacebookRegistration {
 		return driver;
 	}
 
-	void fbRegistrationForm(WebDriver driver) {
+	void fbRegistrationForm(WebDriver driver){
 		driver.get("https://www.facebook.com/r.php");
 		try {
 			Thread.sleep(1000);
@@ -35,7 +34,8 @@ public class FacebookRegistration {
 			driver.findElement(By.name("reg_email__")).sendKeys("234234324sfsdf234234");
 			driver.findElement(By.name("reg_passwd__")).sendKeys("Don't Ask");
 
-			System.out.println(driver.getPageSource().contains("Date of birth") ? "Page contains Date Of Birth" : "Page doesnot contains Date Of Birth");
+			System.out.println(driver.getPageSource().contains("Date of birth") ? "Page contains Date Of Birth" : "Page doesnot contains Date Of Birth");			
+			
 			Select selectDate = new Select(driver.findElement(By.xpath("//select[@title='Day']")));
 			selectDate.selectByValue("14");
 			Select selectMonth = new Select(driver.findElement(By.xpath("//select[@title='Month']")));
@@ -43,22 +43,25 @@ public class FacebookRegistration {
 			Select selectYear = new Select(driver.findElement(By.xpath("//select[@title='Year']")));
 			selectYear.selectByVisibleText("1992");
 
-			WebElement gender;
-
-			if(driver.findElement(By.xpath("//input[@name='sex' and @value='1']/..//label")).getText().equalsIgnoreCase("Female")) {
-				gender = driver.findElement(By.xpath("//input[@name='sex' and @value='1']"));
-				gender.click();
-			}
-			else if(driver.findElement(By.xpath("//input[@name='sex' and @value='2']/..//label")).getText().equalsIgnoreCase("Female")) {
-				gender = driver.findElement(By.xpath("//input[@name='sex' and @value='2']"));
-				gender.click();
-			}
-			else {
-				gender = driver.findElement(By.xpath("//input[@name='sex' and @value='3']"));
-				gender.click();
-			}
-
-			System.out.println(gender.isSelected() ? "Gender selected successfully!!" : "Gender incorrectly selected!!");
+			driver.findElement(By.xpath("//a[@id='gender-help']")).click();
+			System.out.println(driver.findElement(By.xpath("//div[@aria-label='Explanation tooltip for gender options during registration']/..//div/div")).getText().equals("You can change who sees your gender on your profile later. Select Custom to choose another gender, or if you'd rather not say.") ? "Gender tooltip data is correct" : "Gender tooltip data is incorrect");
+			driver.findElement(By.xpath("//a[@class='_42ft _4jy0 layerCancel uiOverlayButton _4jy3 _4jy1 selected _51sy']")).click();
+			
+			driver.findElement(By.xpath("//input[@name='sex']/..//label[text()='Female']")).click();
+			System.out.println(driver.findElement(By.xpath("//label[text()='Female']/..//input[@name='sex']")).isSelected() 
+					? "Gender Female selected successfully!!" : "Gender Female not selected!!");
+			
+			driver.findElement(By.xpath("//input[@name='sex']/..//label[text()='Male']")).click();
+			System.out.println(driver.findElement(By.xpath("//label[text()='Male']/..//input[@name='sex']")).isSelected() 
+					? "Gender Male selected successfully!!" : "Gender Male not selected!!");
+			
+			driver.findElement(By.xpath("//input[@name='sex']/..//label[text()='Custom']")).click();
+			Select selectPronoun = new Select(driver.findElement(By.name("preferred_pronoun")));
+			selectPronoun.selectByVisibleText("She: \"Wish her a happy birthday!\"");
+			driver.findElement(By.name("custom_gender")).sendKeys("Custom Gender");	
+			System.out.println(driver.findElement(By.xpath("//label[text()='Custom']/..//input[@name='sex']")).isSelected() 
+					? "Gender Custom selected successfully!!" : "Gender Custom not selected!!");
+			
 			System.out.println((selectDate.getFirstSelectedOption().getText().equals("14") && selectYear.getFirstSelectedOption().getText().equals("1992")) 
 					? "Date Of Birth is selected correctly" : "Date Of Birth is not correctly selected");
 
@@ -67,6 +70,8 @@ public class FacebookRegistration {
 		else {
 			System.out.println("Url not reloaded correctly");
 		}
+		
+		driver.quit();
 	}
 
 	public static void main(String[] args) {
