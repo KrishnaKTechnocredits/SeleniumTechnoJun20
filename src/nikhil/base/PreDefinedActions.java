f301a4924fe41002fe9c0a0455228ec2620f6eb5
Nikhil M <nikhil.mahabaleshwarkar@gmail.com>
2020-08-20 23:49:52 +0530

    Nikhil_Assignment_5_Done

---
+++ src/nikhil/base/PreDefinedActions.java
@@ -0,0 +1,20 @@
+package nikhil.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PreDefinedActions {
+	public WebDriver start(String url) {
+		String os = System.getProperty("os.name").toLowerCase();
+		System.out.println("Operating System : "+os);
+		String path = os.contains("windows") ? "./resources/windows/chromedriver.exe"
+				: os.contains("mac") ? "./resources/mac/chromedriver" : null;
+		
+		System.setProperty("webdriver.chrome.driver", path);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		
+		return driver;
+	}
+}
