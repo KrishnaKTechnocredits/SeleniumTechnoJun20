44b449b7ca22b0ffdc37cba82aa7384b2b05c275
Anup Sahoo <anupsahoo1408@gmail.com>
2020-08-23 03:24:54 -0400

    Anup_Coding_Exam_1_done_80mins

--- src/anup/basics/PredefinedActions.java
+++ src/anup/basics/PredefinedActions.java
@@ -13,8 +13,11 @@ public class PredefinedActions {
 		System.setProperty("webdriver.chrome.driver", path);
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(url);
+		driver.navigate().to(url);
 		return driver;
+	}
+	public WebDriver  start() {
+		return start("file:///Users/amitarout/Desktop/TechnoGitProject/SeleniumJun20/SeleniumTechnoJun20/resources/forms/Exam-1.html");
 	}
 
 }
