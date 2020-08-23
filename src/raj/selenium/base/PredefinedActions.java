c2380f031927eb4ee194150dc1d15803f3ea006f
Raj Adatiya <rajadatiya35@gmail.com>
2020-08-23 11:41:34 +0530

    Base_Package_Commit

---
+++ src/raj/selenium/base/PredefinedActions.java
@@ -0,0 +1,17 @@
+package raj.selenium.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PredefinedActions {
+	public WebDriver start(String url) {
+		String osName = System.getProperty("os.name").toLowerCase();
+		String path = osName.contains("windows") ? "./resources/windows/chromedriver.exe"
+				: osName.contains("mac") ? "./resources/mac/chromedriver" : null;
+		System.setProperty("webdriver.chrome.driver", path);
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		return driver;
+	}
+}
