fe2166f6f42e29ff7234b07d9972210827a19d6b
Mahesh Kumavat <mkumavat31@gmail.com>
2020-08-23 12:33:03 +0530

    Updated as per comment from Harsh

--- src/mahesh/Exam/CodingExam1.java
+++ src/mahesh/Exam/CodingExam1.java
@@ -14,19 +14,13 @@ public class CodingExam1 extends PredefinedActions{
 		driver = start(url);
 	}
 	
-	void displayMissingLinks(int countOfRows) {
-		for (int index =1; index<=countOfRows;index++) {
-			if (driver.findElement(By.xpath("//tbody/tr["+index+"]/td[3]/a")).getAttribute("href").equals("")) {
-				System.out.println("link is missing for company: " + driver.findElement(By.xpath("//tbody/tr["+index+"]/td[2]")).getText() + " and name: " + driver.findElement(By.xpath("//tbody/tr["+index+"]/td[1]")).getText());
-			}
-		}
-	}
-	
-	void displayTitleMismatch(int countOfRows) {
+	void linkValidation() {
+		int countOfRows = driver.findElements(By.xpath("//tbody/tr")).size();
 		for (int index =1; index<=countOfRows;index++) {
 			driver.manage().timeouts().implicitlyWait(5000,TimeUnit.MILLISECONDS);
 			String webpageTitle = driver.findElement(By.xpath("//tbody/tr["+index+"]/td[4]")).getText();
 			String companyName = driver.findElement(By.xpath("//tbody/tr["+index+"]/td[2]")).getText();
+			String name = driver.findElement(By.xpath("//tbody/tr["+index+"]/td[1]")).getText();
 			WebElement linkElement = driver.findElement(By.xpath("//tbody/tr["+index+"]/td[3]/a"));
 			if (!linkElement.getAttribute("href").equals("")) {
 				linkElement.click();
@@ -34,16 +28,17 @@ public class CodingExam1 extends PredefinedActions{
 					System.out.println("Title is not matching for company: " + companyName);
 				}
 				driver.navigate().back();
+			}else {
+				System.out.println("link is missing for company: " + companyName + " and name: " + name);
 			}
+				
 		}
 	}
 	
 	public static void main(String[] args) {
 		CodingExam1 codingExam1 = new CodingExam1();
 		codingExam1.setUp("file:///D:/Technocredit/Project/TechnoGitProject/SeleniumTechnoJun20/resources/forms/Exam-1.html");
-		int countOfRows = codingExam1.driver.findElements(By.xpath("//tbody/tr")).size();
-		codingExam1.displayMissingLinks(countOfRows);
-		codingExam1.displayTitleMismatch(countOfRows);
+		codingExam1.linkValidation();
 		codingExam1.driver.close();
 	}
 }
