1f0c5ea3050e52f506e25bc900e962fe2d4310d8
Barkha Patle <barkhapatle01@gmail.com>
2020-08-27 22:44:05 +0530

    Barkha_Assignment_12

---
+++ src/barkha/DragAndDrop.java
@@ -0,0 +1,55 @@
+package barkha;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import barkha_base.PredefinedFunctions;
+
+public class DragAndDrop extends PredefinedFunctions {
+	
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	void setUP(String URL) {
+		driver=start(URL);
+	}
+	
+	void navigateToPage() {
+		driver.findElement(By.xpath("//a[text()='Events']")).click();
+		
+		wait=new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='draggable']")));
+	}
+	
+	void dragAndDropFunction() {
+		WebElement source=driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement destination=driver.findElement(By.xpath("//div[@id='droptarget']"));
+		
+		Actions actions=new Actions(driver) ;
+		actions.dragAndDrop(source, destination).build().perform();
+		
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='droptarget']")));
+		String actualText=destination.getText();
+		
+		if (actualText.equals("You did great!"))
+			System.out.println("Test Case PASS: Source successfully dragged into the target.");
+		else
+			System.out.println("Test Case FAIL.");
+	}
+	
+	void tearDown() {
+		driver.close();
+	}
+	
+	public static void main(String[] args) {
+		DragAndDrop dragAndDrop=new DragAndDrop();
+		dragAndDrop.setUP("https://demos.telerik.com/kendo-ui/dragdrop/index");
+		dragAndDrop.navigateToPage();
+		dragAndDrop.dragAndDropFunction();
+		dragAndDrop.tearDown();
+	}
+}
---
+++ src/barkha/MouseHover.java
@@ -0,0 +1,49 @@
+package barkha;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import barkha_base.PredefinedFunctions;
+
+public class MouseHover extends PredefinedFunctions {
+	
+	WebDriver driver;
+	
+	void setUp(String URL) {
+		driver=start(URL);
+	}
+	
+	void mouseHoverOnElement() {
+		Actions actions=new Actions(driver);
+		actions.moveToElement(driver.findElement(By.xpath("//div[@id='nav-tools']/a[2]"))).build().perform();
+		
+		driver.findElement(By.xpath("//span[text()='Your Account']")).click();
+		
+		WebDriverWait wait=new WebDriverWait(driver,10);
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='a-row a-spacing-base']/h1")));
+		
+		String actualText=driver.findElement(By.xpath("//div[@class='a-row a-spacing-base']/h1")).getText();
+		
+		if (actualText.equals("Your Account"))
+				
+				
+				
+			System.out.println("Test Case PASS: Successfully landed to Account Screen.");
+		else
+			System.out.println("Test Case FAIL.");
+	}
+	
+	void tearDown() {
+		driver.close();
+	}
+	
+	public static void main(String[] args) {
+		MouseHover mouseHover=new MouseHover();
+		mouseHover.setUp("https://www.amazon.in/");
+		mouseHover.mouseHoverOnElement();
+		mouseHover.tearDown();
+	}
+}
