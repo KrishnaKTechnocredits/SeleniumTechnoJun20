9bdde2dc529fa9bc92c64a6cf2ee3062b9f89fc4
Krishna Kanani <kananikrishna427@gmail.com>
2020-08-30 12:37:22 +0530

    TestNG

--- src/technoCredits/basics/testngdemo/DemoTest.java
+++ src/technoCredits/basics/testngdemo/DemoTest.java
@@ -1,11 +1,16 @@
-package technoCredits.basics;
+package technoCredits.basics.testngdemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class DemoTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void firstTest() {
+		String expectedTitle = "Login Signup Demo1";
+		
 		String os = System.getProperty("os.name").toLowerCase();
 		System.out.println("os : "+ os);
 		String path = os.contains("windows") ? "./resources/windows/chromedriver.exe"
@@ -14,8 +19,11 @@ public class DemoTest {
 		System.setProperty("webdriver.chrome.driver", path);
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://automationbykrishna.com/");
+		String actualTitle = driver.getTitle();
+		System.out.println("------Before");
+		
 		System.out.println("AutomationByKrishna website open successfully.");
 		driver.quit();
-
+		Assert.assertEquals(actualTitle, expectedTitle); // Hard Assert
 	}
 }
---
+++ src/technoCredits/basics/testngdemo/TwoDArrayDemo.java
@@ -0,0 +1,13 @@
+package technoCredits.basics.testngdemo;
+
+public class TwoDArrayDemo {
+
+	public static void main(String[] args) {
+		Object[][] data = new Object[2][3];
+		data[0][0] = "mkanani";
+		data[0][1] = "mkanani122345";
+		data[0][2] = "Success!";
+		
+		System.out.println(data[0][0]);
+	}
+}
