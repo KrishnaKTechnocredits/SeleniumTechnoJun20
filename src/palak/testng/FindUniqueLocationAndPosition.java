a440acd64ca5cdcc52f5f6610f6deebea663b33a
Palak Soni <palaksoni1907@gmail.com>
2020-09-07 20:22:41 +0530

    Palak_Assingment_16

---
+++ src/palak/testng/FindUniqueLocationAndPosition.java
@@ -0,0 +1,97 @@
+/*Assignment - 16 : 6th Sep'2020
+
+1) Find Unique Office location
+2) Find Unique Position
+*/
+package palak.testng;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import palak.base.PredefinedActions;
+
+public class FindUniqueLocationAndPosition extends PredefinedActions {
+	WebDriver driver;
+	WebDriverWait wait;
+	JavascriptExecutor js;
+	
+	@BeforeTest
+	void setUp() {
+		driver = start("https://editor.datatables.net/examples/extensions/excel");
+	}
+	
+	@Test(priority =1 )
+	void findUniqueOfficeLocation() {
+		Set<String> officeLocationSet = new LinkedHashSet<String>();
+		wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[text()='Ashton']")));
+		WebElement scrollElement = driver.findElement(By.xpath("//td[text()='Ashton']"));
+		
+		js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView()", scrollElement);
+		
+		int size = driver.findElements(By.xpath("//span/a[@tabindex='0']")).size();
+	
+			for (int outerIndex = 1; outerIndex <= size; outerIndex++) {
+				//Get Row Size - 10, at last  page 7
+				int rowSize = driver.findElements(By.xpath("//tr[@role='row']//td[5]")).size();
+				
+				for (int index = 1; index <= rowSize; index++) {
+					WebElement officeLocationElement = driver.findElement(By.xpath("//tbody//tr[" + index + "]//td[5]"));
+					officeLocationSet.add(officeLocationElement.getText());
+				}
+				//Click on Page element
+				driver.findElement(By.xpath("//span/a[@tabindex='0']["+outerIndex+"]")).click();				
+			}
+			
+		System.out.println("***Unique office Locations List is below***");
+		for (String officeLocation : officeLocationSet) {
+			System.out.println(officeLocation);
+		}
+	}
+
+	@Test(priority = 2)
+	void findUniquePosition() {
+		driver.findElement(By.xpath("//a[@data-dt-idx='1']")).click();
+		Set<String> uniquePositionSet = new LinkedHashSet<String>();
+		
+		wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//td[text()='Ashton']")));
+		WebElement scrollElement = driver.findElement(By.xpath("//td[text()='Ashton']"));
+		
+		js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView()", scrollElement);
+		
+		int size = driver.findElements(By.xpath("//span/a[@tabindex='0']")).size();
+		
+			for (int outerIndex = 1; outerIndex <= size; outerIndex++) {
+				//Get Row Size - 10, at last  page 7
+				int rowSize = driver.findElements(By.xpath("//tr[@role='row']//td[5]")).size();
+				
+				for (int index = 1; index <= rowSize; index++) {
+					WebElement positionElement = driver.findElement(By.xpath("//tbody/tr[" + index + "]//td[4]"));
+					uniquePositionSet.add(positionElement.getText());
+				}
+				//Click on Page element
+				driver.findElement(By.xpath("//span/a[@tabindex='0']["+outerIndex+"]")).click();				
+			}
+		
+		System.out.println("\n***Unique Positions list is below***");
+		for(String uniquePosition : uniquePositionSet)
+			System.out.println(uniquePosition);
+	}
+	@AfterTest
+	void tearDown() {
+		driver.close();
+	}
+}
